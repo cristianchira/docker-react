f02523a0cb97ecbc95cf3e8c25066455f13e09d8
cristianchira <cristianchira@gmail.com>
2022-10-15 14:40:06 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chrys, senior Java Developer!
+          Chrys is senior Java Developer!
         </p>
         <a
           className="App-link"
